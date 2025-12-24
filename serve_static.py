c72bb7f55dbc76821139c0de0c602e604f606021
Shiina <quangtungnguyen613@gmail.com>
2025-12-24 13:40:27 +0700

    done function first

---
+++ serve_static.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8080
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print(f"Serving at http://127.0.0.1:{PORT}")
+        httpd.serve_forever()
